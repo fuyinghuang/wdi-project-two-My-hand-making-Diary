df54c0e91841033297f7a2db81f1f44a07d8a483
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-10-28 22:21:13 +0000

    create show page

--- db/data.js
+++ db/data.js
@@ -3,7 +3,7 @@ const handmadesArray = [
     id: '0',
     name: 'Macaroon',
     tools: [ 'crochet (4mm)', 'cotton yarn purple 100g', 'cotton yarn yellow 100g','cotton yarn white 30g','cotton grean 100g','cotton 50g'],
-    image: 'https://i-h2.pinimg.com/564x/07/42/4c/07424c9822df5b6469307e59b32945f3.jpg',
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/10924177_1522118381448470_356671844929452791_o.jpg?_nc_cat=102&_nc_ht=scontent-lhr3-1.xx&oh=60a3150ec486e5667922748174d9d83e&oe=5C3DBDD9',
     concept: 'I want it forever.'
   },
   {
@@ -22,6 +22,20 @@ const handmadesArray = [
   },
   {
     id: '3',
+    name: 'For Totoro',
+    tools: [ 'crochet (4mm)', 'merino wool dark puprple 20g', 'merino wool spotted bedge 10g','cotton'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11782324_1446546092339033_8248034298653248867_o.jpg?_nc_cat=108&_nc_ht=scontent-lhr3-1.xx&oh=c48a572da1a2fb0e8228661f29e75485&oe=5C5209A2',
+    concept: 'OMG!!!Super Giant'
+  },
+  {
+    id: '4',
+    name: 'Jesus',
+    tools: [ 'crochet (10mm)', 'grey fabric yarn 100g', 'fabric yarn spotted bedge 50g'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/12513531_1535786233415018_438026075782027639_o.jpg?_nc_cat=109&_nc_ht=scontent-lhr3-1.xx&oh=4d220f1502521d55c0c9893a1c636826&oe=5C85E250',
+    concept: 'OMG!!!Super Giant'
+  },
+  {
+    id: '5',
     name: 'Halloween',
     tools: [ 'crochet (6mm)', 'orange merino wool yarn 300g', 'green merino wool yarn 100g'],
     image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/12194785_1498675603792748_1654328398930223287_o.jpg?_nc_cat=101&_nc_ht=scontent-lhr3-1.xx&oh=78a60dc80920b9fcd2c229969149405c&oe=5C7D977D',
