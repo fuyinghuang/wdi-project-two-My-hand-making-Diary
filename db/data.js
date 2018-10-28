cdf400ffacdbb010d1a18524d84f078a433d5978
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-10-28 16:07:01 +0000

    create data

---
+++ db/data.js
@@ -0,0 +1,33 @@
+const handmadesArray = [
+  {
+    id: '0',
+    name: 'Macaroon',
+    tools: [ 'crochet (4mm)', 'cotton yarn purple 100g', 'cotton yarn yellow 100g','cotton yarn white 30g','cotton grean 100g','cotton 50g'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/10924177_1522118381448470_356671844929452791_o.jpg?_nc_cat=102&_nc_ht=scontent-lhr3-1.xx&oh=60a3150ec486e5667922748174d9d83e&oe=5C3DBDD9',
+    concept: 'I want it forever.'
+  },
+  {
+    id: '1',
+    name: 'Tiny Tiny',
+    tools: [ 'crochet (5mm)', 'hemp rope 200g', 'cotton yarn white 100g', 'brown leather rope 50cm'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11754383_1438118433181799_3642149608190208505_o.jpg?_nc_cat=110&_nc_ht=scontent-lhr3-1.xx&oh=7c07e562005fb4c7c3672a1e6fdc54e3&oe=5C7E52A6',
+    concept: 'Useless is the best.'
+  },
+  {
+    id: '2',
+    name: 'Elephant/s bed',
+    tools: [ 'crochrt (4mm)', 'purple lace yarn 150g'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11412059_1430092863984356_8925411828553317105_o.jpg?_nc_cat=105&_nc_ht=scontent-lhr3-1.xx&oh=ce026de544151be1755fbf65f9f471d0&oe=5C4D4BEB',
+    concept: 'The luxurist bed in the world!!!'
+  },
+  {
+    id: '3',
+    name: 'Halloween',
+    tools: [ 'crochet (6mm)', 'orange merino wool yarn 300g', 'green merino wool yarn 100g'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/12194785_1498675603792748_1654328398930223287_o.jpg?_nc_cat=101&_nc_ht=scontent-lhr3-1.xx&oh=78a60dc80920b9fcd2c229969149405c&oe=5C7D977D',
+    concept: 'I wear pumpkin!!!'
+  }
+];
+module.exports= {
+  handmade: handmadesArray
+};
