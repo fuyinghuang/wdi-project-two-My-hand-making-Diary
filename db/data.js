41c962cbb415c81785e93d9c2ba340721abaf709
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-10-29 01:36:39 +0000

    updated data.js

--- db/data.js
+++ db/data.js
@@ -1,45 +1,67 @@
 const handmadesArray = [
   {
     id: '0',
-    name: 'Macaroon',
-    tools: [ 'crochet (4mm)', 'cotton yarn purple 100g', 'cotton yarn yellow 100g','cotton yarn white 30g','cotton grean 100g','cotton 50g'],
+    name: 'Sweets in the forest',
+    tools: [ 'crochet (4mm)'],
+    materials: ['cotton yarn purple 100g', 'cotton yarn yellow 100g','cotton yarn white 30g','cotton grean 100g','cotton 50g'],
     image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/10924177_1522118381448470_356671844929452791_o.jpg?_nc_cat=102&_nc_ht=scontent-lhr3-1.xx&oh=60a3150ec486e5667922748174d9d83e&oe=5C3DBDD9',
     concept: 'I want it forever.'
   },
   {
     id: '1',
     name: 'Tiny Tiny',
-    tools: [ 'crochet (5mm)', 'hemp rope 200g', 'cotton yarn white 100g', 'brown leather rope 50cm'],
+    tools: [ 'crochet (5mm)'],
+    materials: [ 'hemp rope 200g', 'cotton yarn white 100g', 'brown leather rope 50cm'],
     image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11754383_1438118433181799_3642149608190208505_o.jpg?_nc_cat=110&_nc_ht=scontent-lhr3-1.xx&oh=7c07e562005fb4c7c3672a1e6fdc54e3&oe=5C7E52A6',
     concept: 'Useless is the best.'
   },
   {
     id: '2',
-    name: 'Elephant\'s bed',
-    tools: [ 'crochrt (4mm)', 'purple lace yarn 150g'],
+    name: 'Elephant Web',
+    tools: [ 'crochrt (4mm)'],
+    materials: [ 'crochrt (4mm)', 'purple lace yarn 150g'],
     image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11412059_1430092863984356_8925411828553317105_o.jpg?_nc_cat=105&_nc_ht=scontent-lhr3-1.xx&oh=ce026de544151be1755fbf65f9f471d0&oe=5C4D4BEB',
     concept: 'The luxurist bed in the world!!!'
   },
   {
     id: '3',
-    name: 'For Totoro',
-    tools: [ 'crochet (4mm)', 'merino wool dark puprple 20g', 'merino wool spotted bedge 10g','cotton'],
-    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11782324_1446546092339033_8248034298653248867_o.jpg?_nc_cat=108&_nc_ht=scontent-lhr3-1.xx&oh=c48a572da1a2fb0e8228661f29e75485&oe=5C5209A2',
-    concept: 'OMG!!!Super Giant'
+    name: 'Let\'s Go Picnic',
+    tools: [ 'crochet (7mm)'],
+    materials: [ 'Raffia mixed green 250g', 'wooden button x1'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11212616_1425049907821985_5054781303753966539_o.jpg?_nc_cat=100&_nc_ht=scontent-lhr3-1.xx&oh=fa7187946d19a73e01f8709d1cf3aeac&oe=5C835571',
+    concept: 'I wear pumpkin!!!'
   },
   {
     id: '4',
-    name: 'Jesus',
-    tools: [ 'crochet (10mm)', 'grey fabric yarn 100g', 'fabric yarn spotted bedge 50g'],
+    name: '8 Wonders in the World',
+    tools: [ 'crochet (10mm)'],
+    materials: [ 'crochet (10mm)', 'grey fabric yarn 100g', 'fabric yarn spotted bedge 50g'],
     image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/12513531_1535786233415018_438026075782027639_o.jpg?_nc_cat=109&_nc_ht=scontent-lhr3-1.xx&oh=4d220f1502521d55c0c9893a1c636826&oe=5C85E250',
     concept: 'OMG!!!Super Giant'
   },
   {
     id: '5',
     name: 'Halloween',
-    tools: [ 'crochet (6mm)', 'orange merino wool yarn 300g', 'green merino wool yarn 100g'],
+    tools: [ 'crochet (6mm)'],
+    materials: [ 'orange merino wool yarn 300g', 'green merino wool yarn 100g'],
     image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/12194785_1498675603792748_1654328398930223287_o.jpg?_nc_cat=101&_nc_ht=scontent-lhr3-1.xx&oh=78a60dc80920b9fcd2c229969149405c&oe=5C7D977D',
     concept: 'I wear pumpkin!!!'
+  },
+  {
+    id: '6',
+    name: 'Smell My Sacks',
+    tools: [ 'rod needle (8mm) x4','wheel needle (10mm)'],
+    materials: [ 'orange merino wool yarn 300g', 'green merino wool yarn 100g'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/12029819_1485411005119208_8345006872718668448_o.jpg?_nc_cat=106&_nc_ht=scontent-lhr3-1.xx&oh=6e0b223c068145f9d32a6b50c94f31fb&oe=5C3F646E',
+    concept: 'I wear pumpkin!!!'
+  },
+  {
+    id: '7',
+    name: 'Para Totoro',
+    tools: [ 'crochet (4mm)'],
+    materials: [ 'merino wool dark puprple 20g', 'merino wool spotted bedge 10g','cotton'],
+    image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11782324_1446546092339033_8248034298653248867_o.jpg?_nc_cat=108&_nc_ht=scontent-lhr3-1.xx&oh=c48a572da1a2fb0e8228661f29e75485&oe=5C5209A2',
+    concept: 'OMG!!!Super Giant'
   }
 ];
 module.exports= {
