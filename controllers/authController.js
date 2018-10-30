d66760a206187ff3660ae0c96eb1de6ee05457e0
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-10-30 00:22:53 +0000

    create sign up/in button linked to register and log in form

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,11 +6,8 @@ function registerFormRoute(req, res) {
 }
 
 function registerRoute(req, res) {
-  // Create a new user
-  // req.body contains the data from the registration form
   User.create(req.body).then(result => {
     console.log('User created', result);
-    // redirect to the home page
     res.redirect('/');
   });
 }
@@ -22,20 +19,13 @@ function loginFormRoute(req, res) {
 // Login route
 function loginRoute(req, res) {
   console.log('User is logging in', req.body);
-  // Process the login.
-  // Check for an existing user
   User.findOne({ email: req.body.email }).then(result => {
-    // Hopefully, result contains a user
     if (!result){
-      // if there is no user
       res.redirect('/login');
     }else {
-      // We've found a user in the database! Write the user's
-      // ID into their locker (session).
-      // (req.session is the locker)
       console.log('Logging in', result);
       req.session.userId = result._id;
-      // console.log('userid is',req.session.userId) ;
+      console.log('userid is',req.session.userId) ;
       res.redirect('/');
     }
   });
