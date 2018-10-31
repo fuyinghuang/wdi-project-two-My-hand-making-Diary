03646b95036356396c7ae874fd7d39efce8866a7
huangfuin11 <huangfuin1101@yahoo.com.tw>
2018-10-31 16:41:24 +0000

    center welcome user

--- db/create-data.js
+++ db/create-data.js
@@ -12,10 +12,15 @@ const userData = [{
   email: 'd@d.com',
   password: 'd'
 }, {
-  username: 'matt',
-  email: 'm@m.com',
-  password: 'm'
-}];
+  username: 'Fu ying',
+  email: 'f@f.com',
+  password: 'f'
+}, {
+  username: 'Cua cua',
+  email: 'c@c.com',
+  password: 'c'
+}
+];
 
 User
   .create(userData)
@@ -45,7 +50,7 @@ User
           materials: [ 'crochrt (4mm)', 'purple lace yarn 150g'],
           image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11412059_1430092863984356_8925411828553317105_o.jpg?_nc_cat=105&_nc_ht=scontent-lhr3-1.xx&oh=ce026de544151be1755fbf65f9f471d0&oe=5C4D4BEB',
           concept: 'The luxurist bed in the world!!! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          addedBy: users[0]
+          addedBy: users[1]
         },
         {
           name: 'Let\'s Go Picnic',
@@ -53,7 +58,7 @@ User
           materials: [ 'Raffia mixed green 250g', 'wooden button x 1'],
           image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11212616_1425049907821985_5054781303753966539_o.jpg?_nc_cat=100&_nc_ht=scontent-lhr3-1.xx&oh=fa7187946d19a73e01f8709d1cf3aeac&oe=5C835571',
           concept: 'Picnic time!!! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          addedBy: users[1]
+          addedBy: users[2]
         },
         {
           name: 'Smell My Socks',
@@ -78,7 +83,7 @@ User
           materials: [ 'merino wool dark puprple 20g', 'merino wool spotted bedge 10g'],
           image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/11782324_1446546092339033_8248034298653248867_o.jpg?_nc_cat=108&_nc_ht=scontent-lhr3-1.xx&oh=c48a572da1a2fb0e8228661f29e75485&oe=5C5209A2',
           concept: 'OMG!!!Super Giant',
-          addedBy: users[0]
+          addedBy: users[2]
         },
         {
           name: 'Fuji Mountain ',
@@ -86,7 +91,7 @@ User
           materials: [ 'blue wool yarn 80g','white cotton yarn 40g' ],
           image: 'https://scontent-lht6-1.xx.fbcdn.net/v/t31.0-8/12185426_1504421616551480_602440101276934445_o.jpg?_nc_cat=107&_nc_ht=scontent-lht6-1.xx&oh=a4ca81143b9595656701c3301dc66601&oe=5C4009E0',
           concept: 'Holiday, I miss you.....',
-          addedBy: users[1]
+          addedBy: users[2]
         },
         {
           name: 'A Pair of Purple',
@@ -94,7 +99,7 @@ User
           materials: [ 'purple wool yarn 200g'],
           image: 'https://scontent-lht6-1.xx.fbcdn.net/v/t31.0-8/12473520_1534266820233626_2103542678007377958_o.jpg?_nc_cat=109&_nc_ht=scontent-lht6-1.xx&oh=92f386292c45215ae7d17b5b07c28f22&oe=5C43DFF4',
           concept: 'Ces\'s moi, télégant...',
-          addedBy: users[0]
+          addedBy: users[1]
         },
         {
           name: '8 Wonders in the World',
@@ -102,7 +107,7 @@ User
           materials: [ 'crochet (10mm)', 'grey fabric yarn 100g', 'fabric yarn spotted bedge 50g'],
           image: 'https://scontent-lhr3-1.xx.fbcdn.net/v/t31.0-8/12513531_1535786233415018_438026075782027639_o.jpg?_nc_cat=109&_nc_ht=scontent-lhr3-1.xx&oh=4d220f1502521d55c0c9893a1c636826&oe=5C85E250',
           concept: 'OMG!!!Super Giant',
-          addedBy: users[1]
+          addedBy: users[0]
         },
         {
           name: 'Early Bird',
@@ -126,7 +131,7 @@ User
           materials: [ 'brown wool yarn 30g','dark brown wool yarn 10g', 'black ponytailer x 1 ' ],
           image: 'https://scontent-lht6-1.xx.fbcdn.net/v/t31.0-8/11893974_1454586101535032_8760412356355386936_o.jpg?_nc_cat=111&_nc_ht=scontent-lht6-1.xx&oh=fa4f4efe43a82c5600b60e648b13f97d&oe=5C43039C',
           concept: 'Holiday, I miss you.....',
-          addedBy: users[0]
+          addedBy: users[2]
         },
         {
           name: 'Candy House',
@@ -134,7 +139,7 @@ User
           materials: [ 'cotton yarn pink 100g', 'cotton yarn tiffiny green 150g'],
           image: 'https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/12417639_1543615409298767_3411523855612750212_n.jpg?_nc_cat=104&_nc_ht=scontent-lht6-1.xx&oh=043e642181495a369565099231099bb1&oe=5C4CB207',
           concept: 'Give me all the sweets...',
-          addedBy: users[1]
+          addedBy: users[0]
         },
         {
           name: 'Bunny',
